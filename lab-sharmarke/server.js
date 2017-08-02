04eba44089369aad8aa5631eea1bb27deea034db
sharmarke <smarky292@gmail.com>
2017-08-02 12:41:38 -0700

    server running

---
+++ lab-sharmarke/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const debug = require('debug')('note:server');
+const listRouter = require('./route/list-route.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = 'mongodb://localhost/listofnotes';
+
+app.listen(PORT, () => {
+  debug(`listening on ${PORT}`);
+});
