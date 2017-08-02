7cbe806146520277e120731ecc5d241d3c2514ed
sharmarke <smarky292@gmail.com>
2017-08-02 13:23:13 -0700

    renamed files and set up car routes

---
+++ lab-sharmarke/route/car-route.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const Car = require('../model/car.js');
+const carRouter = module.exports = new Router();
+const debug = require('debug')('car:car-router');
+
+carRouter.post('/api/car', jsonParser, function(req, res, next) {
+  debug('POST: /api/car');
+  res.body.timestamp = new Date();
+  new Car(req.body).save()
+  .then( car => res.json( ))
+  .catch(next);
+});
+
+carRouter.get('/api/car/:id', function(req, res, next) {
+  debug('GET: /api/car');
+  Car.findById(req.params.id)
+  .then( car => res.json(car))
+  .catch(next);
+});
+
+carRouter.delete('/api/car/:id', function(req, res, next) {
+  debug('DELETE: /api/car');
+  Car.findByIdAndRemove(req.params.id, req.body)
+  .then(car => res.json(car))
+  .catch(next);
+});
--- lab-sharmarke/server.js
+++ lab-sharmarke/server.js
@@ -5,12 +5,12 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
-const debug = require('debug')('note:server');
-const listRouter = require('./route/list-route.js');
+const debug = require('debug')('car:server');
+const carRouter = require('./route/car-route.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = 'mongodb://localhost/listofnotes';
+const MONGODB_URI = 'mongodb://localhost/listofcarss';
 
 app.listen(PORT, () => {
   debug(`listening on ${PORT}`);
